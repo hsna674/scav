445ad06271d0aad71a837ec33a7e6c0207b6fabd
Ansh Agrawal <hsna.agrawal@gmail.com>
2025-08-24 08:59:43 -0400

    more security patchs

--- hunt/settings/__init__.py
+++ hunt/settings/__init__.py
@@ -191,6 +191,17 @@ MIN_REQUEST_TIME = 10
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Security settings for production
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = False  # Set to True in production with HTTPS
+SESSION_COOKIE_SECURE = False  # Set to True in production with HTTPS
+CSRF_COOKIE_SECURE = False  # Set to True in production with HTTPS
+
 try:
     from .secret import *  # noqa
 except ImportError:
