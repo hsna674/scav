714077225f12cfcec3cac6599a1e2cef2b313572
Ansh Agrawal <hsna.agrawal@gmail.com>
2025-09-01 11:14:22 -0400

    fix static files and add challenge button

--- hunt/settings/__init__.py
+++ hunt/settings/__init__.py
@@ -180,7 +180,7 @@ STATIC_ROOT = BASE_DIR / "serve"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 # 5 second delay between each request
